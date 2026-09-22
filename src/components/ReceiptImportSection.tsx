9a9c3330749c9514f946580a1744e33a6bd1fd89
Estilo_Futbol <137332042+EstiloFutbol@users.noreply.github.com>
2026-09-22 20:11:00 +0200

    fix: corregir creación de línea manual

--- src/components/ReceiptImportSection.tsx
+++ src/components/ReceiptImportSection.tsx
@@ -189,7 +189,7 @@ export default function ReceiptImportSection() {
 
   function blankLine(index: number): ReviewLine {
     return {
-      id: lineId(current.length),
+      id: lineId(index),
       rawName: '',
       translatedName: '',
       ingredientId: '',
